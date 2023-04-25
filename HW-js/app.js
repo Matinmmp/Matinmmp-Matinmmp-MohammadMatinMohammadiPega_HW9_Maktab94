1d84241c36c6e1f0732bfa71786ad67672b7ba81
Matinmmp1381 <matinmmp1381@gmail.com>
2023-04-25 09:10:30 -0700

    solve second question

--- HW-js/app.js
+++ HW-js/app.js
@@ -1,9 +1,45 @@
-function test(num = 1) {
-    console.log(typeof num)
-    console.log(num);
-}
+// function test(num = 1) {
+//     console.log(typeof num)
+//     console.log(num);
+// }
 
-test();             //number
-test(undefined);    //number
-test('');           //string
-test(null);         //object
+// test();             //number
+// test(undefined);    //number
+// test('');           //string
+// test(null);         //object
+
+
+
+
+//  2
+// function PassbyReference(obj) {
+//     let tmp = obj.a;
+//     obj.a = obj.b;
+//     obj.b = tmp;
+//     console.log(`Inside Pass By Reference Function -> a = ${obj.a} b = ${obj.b}`);
+// }
+
+// let obj = { a: 10, b: 20 };
+
+// console.log(`Before calling Pass By Reference Function -> a = ${obj.a} b = ${obj.b}`);
+
+// PassbyReference(obj);
+
+// console.log(`After calling Pass By Reference Function -> a = ${obj.a} b = ${obj.b}`);
+
+
+
+// function PassbyReference(obj) {
+//     let tmp = obj.a;
+//     obj.a = obj.b;
+//     obj.b = tmp;
+//     console.log(`Inside Pass By Reference Function -> a = ${obj.a} b = ${obj.b}`);
+// }
+
+// let obj = { a: 10, b: 20 };
+
+// console.log(`Before calling Pass By Reference Function -> a = ${obj.a} b = ${obj.b}`);
+
+// PassbyReference({...obj});
+
+// console.log(`After calling Pass By Reference Function -> a = ${obj.a} b = ${obj.b}`);
